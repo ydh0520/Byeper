deca95b4c1a5dadb99365e79d27c8b4ca8a5a2e0
김동욱 <gustkdxo007@gmail.com>
2020-10-26 11:18:08 +0900

    :pencil: Add eslint rules

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,6 +14,9 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    quotes: ["error", "double"],
+    camelcase: "off",
+    "@typescript-eslint/camelcase": "off",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
