e8b05c84a765ac464818765af0a7d035428138ce
김원호 <ho0707@naver.com>
2020-10-27 13:47:22 +0900

    :art: capture speed upgrade

--- Capture/video_capture.py
+++ Capture/video_capture.py
@@ -2,46 +2,41 @@ import cv2
 import numpy as np
 import os
 from glob import glob
-import datetime
-# video_path = 'algo_1.mp4'
+
+
 video_paths = glob('*.mp4')
-print(video_paths)
-now = datetime.datetime.now()
 for video_path in video_paths:
     os.system(f'mkdir images\\{video_path[:-4]}')
     count = 0
     vidcap = cv2.VideoCapture(video_path)
+    vidcap.set(3, 400)
+    vidcap.set(4, 300)
     flag = 0
     stack = 0
+    frame = 0
+    ret, prev = vidcap.read()
     while(vidcap.isOpened()):
+        vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame)
         ret, image = vidcap.read()
-        # vidcap.grab(20)
         if not ret:
-            if flag == vidcap.get(1):
-                stack += 1
-                if stack > 100:
-                    break
+            stack += 1
+            if stack > 20:
+                break
             continue
+        frame += 150
         stack = 0
-        flag = vidcap.get(1)
-        if(int(vidcap.get(1)) == 1):
-            prev = image
-            height, width, channel = prev.shape
-        elif(int(vidcap.get(1)) % 300 == 0):
-            prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
-            image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            diff = np.subtract(prev_gray, image_gray, dtype=np.int16)
-            diff = np.abs(diff)
-            diff = cv2.resize(diff, (400, 300))
-            sum_diff = np.sum(diff>10)
-            # print(sum_diff)
-            if sum_diff > 10000:
-                print('Saved frame number : ' + str(int(vidcap.get(1))))
-                cv2.imwrite("images/{}/{}_{}.jpg".format(video_path[:-4], count, sum_diff), prev)
-                print('Saved frame%d.jpg' % count)
-                count += 1
-            prev = image
+        prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        diff = np.subtract(prev_gray, image_gray, dtype=np.int16)
+        diff = np.abs(diff)
+        diff = cv2.resize(diff, (400, 300))
+        sum_diff = np.sum(diff>10)
+        # print(sum_diff)
+        if sum_diff > 10000:
+            print('Saved frame number : ' + str(frame))
+            cv2.imwrite("images/{}/{}_{}.jpg".format(video_path[:-4], count, sum_diff), prev)
+            print('Saved frame%d.jpg' % count)
+            count += 1
+        prev = image
     cv2.imwrite("images/{}/{}_{}.jpg".format(video_path[:-4], count, sum_diff), prev)
     vidcap.release()
-    print(datetime.datetime.now()-now)
-    break
