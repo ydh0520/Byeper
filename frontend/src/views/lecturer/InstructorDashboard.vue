b7a2ed911116a6bff4e2f4a5a45f09b52058dc79
utaein <uutaein@gmail.com>
2020-10-30 17:41:58 +0900

    :poop: front-inst page 왼쪽 navbar 세팅

---
+++ frontend/src/views/lecturer/InstructorDashboard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-tabs :key="tab.name" v-for="tab in instructorTabs">
+      <v-tab>
+        {{ tab.name }}
+      </v-tab>
+    </v-tabs>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class InstructorDashboard extends Vue {
+  data() {
+    return {
+      instructorTabs: [
+        {
+          name: "댓글"
+        },
+        {
+          name: "오우야"
+        },
+        {
+          name: "타타타"
+        }
+      ]
+    }
+  }
+
+}
+</script>
+
+<style scoped></style>
