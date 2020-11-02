ef5029175fe7b67c561bfdf8528d8a755a4adc23
김동욱 <gustkdxo007@gmail.com>
2020-11-02 17:09:20 +0900

    :sparkles: Add finished lecture skeleton

---
+++ frontend/src/components/dashboard/FinishedLecture.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card class="mx-auto" min-width="344" width="100%" height="250" outlined>
+    <v-card-title>완료한 강의</v-card-title>
+    <v-card-text>완료한 강의 제목</v-card-text>
+    <v-card-text>완료한 강의 제목</v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class FinishedLecture extends Vue {}
+</script>
+
+<style></style>
