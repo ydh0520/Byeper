329bfc0ace92cad480316a692460c777e50e9285
김원호 <ho0707@naver.com>
2020-11-02 10:57:49 +0900

    정규표현식 추가

--- Capture/video_capture.py
+++ Capture/video_capture.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 import pafy
+import re
 
 def imwrite(filename, img, params=None): 
     try: 
@@ -23,11 +24,37 @@ def get_time(sec):
     return hour, minute, sec
 
 def extract_from_youtube_url(youtube_url):
+    index = re.compile('&index=')
+    url = index.search(youtube_url)
+    if url:
+        youtube_url = youtube_url[:url.start()]
+    time = re.compile('&t=')
+    url = time.search(youtube_url)
+    if url:
+        youtube_url = youtube_url[:url.start()]
+        
     video = pafy.new(youtube_url)
     best = video.getbest()
-
     title = video.title
-
+    tmp_title = ''
+    usable1 = re.compile("[가-힣]?")
+    usable2 = re.compile("[a-zA-Z]?")
+    usable3 = re.compile("[!@#$%^()\[\]_+-=}{';.,]?")
+    
+    for char in title:
+        if char == ' ':
+            tmp_title = tmp_title + char
+            continue
+        y1 = usable1.match(char)
+        y2 = usable2.match(char)
+        y3 = usable3.match(char)
+        print(char)
+        print(y1, y2, y3)
+        if y1.end() + y2.end() + y3.end():
+            tmp_title = tmp_title + char
+        
+    title = tmp_title
+    print(title)
     if not(os.path.isdir('images')):
         os.makedirs(os.path.join('images'))
     if not(os.path.isdir('images\\' + title)):
@@ -43,6 +70,7 @@ def extract_from_youtube_url(youtube_url):
     ret, prev = vidcap.read()
     imwrite("images/{}/00h 00m 00s.jpg".format(title), prev)
     while(vidcap.isOpened()):
+        frame += 150
         vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame)
         ret, image = vidcap.read()
         if not ret:
@@ -50,7 +78,6 @@ def extract_from_youtube_url(youtube_url):
             if stack > 5:
                 break
             continue
-        frame += 300
         stack = 0
         prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
         image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -58,13 +85,11 @@ def extract_from_youtube_url(youtube_url):
         diff = np.abs(diff)
         diff = cv2.resize(diff, (400, 225))
         sum_diff = np.sum(diff>10)
-        if sum_diff > 7500:
+        if sum_diff > 2000:
             print('Saved frame number : ' + str(frame))
-            sec = frame // 30
+            sec = frame // 29.97
             h, m, s = get_time(sec)
-            print(sec)
-            print(h, m, s)
-            imwrite("images/{}/{:0>2}h {:0>2}m {:0>2}s.jpg".format(title, h, m, s), image)
+            imwrite("images/{}/{} {:0>2}h {:0>2}m {:0>2}s.jpg".format(title, sum_diff, h, m, s), image)
             count += 1
         prev = image
     # cv2.imwrite("images/{}/{}_{}.jpg".format(title, count, sum_diff), prev)
