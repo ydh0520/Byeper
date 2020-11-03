38217b44e36826af282d3cd2a912466ea4983a09
이연재 <duseh73@gmail.com>
2020-11-03 20:20:04 +0900

    :construtcion: split component

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,28 +1,26 @@
 <template>
-  <div class="home full-height">
-    <v-row class="full-height">
-      <v-row align="start" justify="center">
-        <div>
-          <div
-            style="font-family: 'Black Han Sans', sans-serif; font-size:40px; margin-bottom: 50px;"
-          >
-            <div>
-              <vue-odometer :value="myValue"></vue-odometer>
-            </div>
-          </div>
-        </div>
-      </v-row>
-    </v-row>
+  <div class="full-height">
+    <div
+      style="font-family: 'Black Han Sans', sans-serif; font-size:40px; margin-bottom: 50px;"
+    >
+      <div style="font-size: 25px; margin-right:30px">
+        <vue-odometer :value="myValue"></vue-odometer>
+      </div>
+    </div>
+
+    <all-play-list />
   </div>
 </template>
 
 <script>
 import { Component, Vue } from "vue-property-decorator";
 import VueOdometer from "v-odometer/src";
+import AllPlayList from "@/components/main/AllPlayList.vue";
 
 @Component({
   components: {
-    "vue-odometer": VueOdometer
+    "vue-odometer": VueOdometer,
+    AllPlayList
   }
 })
 export default class Home extends Vue {
