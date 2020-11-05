8d7c16246f3332ddf1d7777567db9616cc33b18a
이연재 <duseh73@gmail.com>
2020-11-05 12:07:03 +0900

    :construction: design one PlayList page

--- frontend/src/views/main/PlayList.vue
+++ frontend/src/views/main/PlayList.vue
@@ -1,10 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <play-list-description />
+    <v-divider
+      class="light"
+      style="font-weight: bold; margin-top: 10px; "
+    ></v-divider>
+    <play-list-videos />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import PlayListDescription from "@/components/main/PlayListDescription.vue";
+import PlayListVideos from "@/components/main/PlayListVideos.vue";
 
-@Component
+@Component({
+  components: {
+    PlayListDescription,
+    PlayListVideos
+  }
+})
 export default class PlayList extends Vue {}
 </script>
