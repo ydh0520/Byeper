c08a2977dd1025ab268566fefd03db848c0f4e33
편재호 <pyoun820@naver.com>
2020-11-05 20:47:48 +0900

    :construction: finish img2text code

--- backend/django/Image2text.py
+++ backend/django/Image2text.py
@@ -1,43 +1,48 @@
 from google.cloud import vision
-import os, cv2
+import os, cv2, io
+import re
 
-def detect_text(path):
-    import io
+def image_processing(path):
+    # /var/file/VIDEO_ID
+    # root = 'C:\\Users\\pyoun\\Desktop\\s03p31b108\\backend\\django\\tmp\\tQHw2EovIOM'
+    imgs = []
+    for f in os.listdir(path):
+        tmp = cv2.imread(os.path.join(path, f), 0)
+        imgs.append(cv2.resize(tmp, (854, 480)))
+    img = cv2.vconcat(imgs)
+    target_dir = path[-11:]
+    tmp_image = os.path.join(path, target_dir+'.png')
+
+    cv2.imwrite(tmp_image, img)
+    
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:\\Users\\pyoun\\Desktop\\pk.json"
     client = vision.ImageAnnotatorClient()
 
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
+    with io.open(tmp_image, 'rb') as image_path:
+        content = image_path.read()
 
     image = vision.Image(content=content)
     response = client.document_text_detection(image=image)
-
     texts = response.text_annotations
+
     sentence = ''
     for text in texts[0].description.split('\n'):
-        # if len(text.replace(' ', '')) < 25: continue
-        print(len(text.replace(' ', '')), text)
-        sentence += text + ' '
-    # print(sentence)
+        text = re.sub(r"\((.*?)\)", '', text)
+        if len(text.replace(' ', '')) > 20: 
+            sentence += text
+            if sentence[-1] not in '?!.': sentence += '.'
+            sentence += ' '
+
     if response.error.message:
         raise Exception(
             '{}\nFor more info on error messages, check: '
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
 
-def combine_image(path):
-    # /var/file/VIDEO_ID
-    # root = 'C:\\Users\\pyoun\\Desktop\\s03p31b108\\backend\\django\\tmp\\tQHw2EovIOM'
-    target_dir = path[:-11]
-    imgs = []
-    for f in os.listdir(path):
-        tmp = cv2.imread(os.path.join(path, f), 0)
-        imgs.append(cv2.resize(tmp, (854, 480)))
-    img = cv2.vconcat(imgs)
-    tmp_image = os.path.join(path, target_dir+'.png')
-    cv2.imwrite(tmp_image, img)
-    detect_text(tmp_image)
     os.remove(tmp_image)
-    return path
+    return sentence
 
-combine_image('C:\\Users\\pyoun\\Desktop\\s03p31b108\\backend\\django\\tmp\\tQHw2EovIOM')
+PATH = 'C:\\Users\\pyoun\\Desktop\\s03p31b108\\backend\\django\\tmp\\tQHw2EovIOM'
+result = image_processing(PATH)
+
+print(result)
