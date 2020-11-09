f0e69d8b659d7f34685f6a4cc5302ce2352024e3
ydh2338 <ydh2338@naver.com>
2020-11-10 00:28:30 +0900

    :sparkles: PlayList repository customQuery

--- Spring/src/main/java/com/ssafy/model/repository/PlaylistRepository.java
+++ Spring/src/main/java/com/ssafy/model/repository/PlaylistRepository.java
@@ -1,9 +1,17 @@
 package com.ssafy.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ssafy.model.dto.PlaylistDto;
 
 public interface PlaylistRepository extends JpaRepository<PlaylistDto, Integer> {
+	@Query(value = "select * from ssafy.playlist where playlist_type = :playlist_type limit :start ,10", nativeQuery = true)
+	public List<PlaylistDto> findPlaylistTeacher(@Param("playlist_type") int playlistType,
+			@Param("start") Integer start);
 
+	public List<PlaylistDto> findbyUserId(String userId);
 }
