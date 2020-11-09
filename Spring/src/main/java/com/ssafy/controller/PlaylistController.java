c2c094c9b7aa8a20c81cdb10f44a2ec82a8e3e8a
ydh2338 <ydh2338@naver.com>
2020-11-09 23:34:07 +0900

    :sparkles: PlayList Controller get playlist

--- Spring/src/main/java/com/ssafy/controller/PlaylistController.java
+++ Spring/src/main/java/com/ssafy/controller/PlaylistController.java
@@ -2,11 +2,16 @@ package com.ssafy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ssafy.model.response.BasicResponse;
 import com.ssafy.model.service.PlaylistService;
 
 @Controller
@@ -17,8 +22,20 @@ public class PlaylistController {
 	private PlaylistService playlistService;
 
 	@GetMapping("/api/public/palylist/list")
-	public Object FindAllPlayList() {
-		return null;
+	public Object FindAllPlayList(@RequestParam int start) {
+		BasicResponse response = new BasicResponse();
+
+		response.data = playlistService.findAllPlaylistList(start);
+
+		if (response.data != null) {
+			response.status = true;
+			response.message = "조회에 성공하였습니다.";
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} else {
+			response.status = false;
+			response.message = "조회에 실패하였습니다.";
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
 	}
 
 	@GetMapping("/api/public/palylist/detail")
@@ -26,8 +43,8 @@ public class PlaylistController {
 		return null;
 	}
 
-	@PutMapping("/api/public/playlist/save")
-	public Object Saveplaylist() {
+	@PutMapping("/api/private/playlist/save")
+	public Object Saveplaylist(@RequestHeader("Authorization") String jwtToken) {
 		return null;
 	}
 
