d84c45f7ed7e28c1b267a541c06f7b40069cda73
이연재 <duseh73@gmail.com>
2020-11-09 20:45:23 +0900

    :construction: setting lecturePage Title

--- frontend/src/views/student/LectureListPage.vue
+++ frontend/src/views/student/LectureListPage.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="lecture-list-page mx-auto mt-10">
-    <div class="text-h3 my-5">
+  <div
+    class="lecture-list-page mx-auto mt-10"
+    style="font-family: 'Do Hyeon', sans-serif !important;"
+  >
+    <div
+      class="text-h3 mb-10 "
+      style="text-align: center; font-family: 'Do Hyeon', sans-serif !important;"
+    >
       {{ this.$route.params.courseName }}
     </div>
     <statistics-by-cource />
