71dff8b0e48d3fd5133a03d6f691d2a6280dbf4b
ydh2338 <ydh2338@naver.com>
2020-11-10 00:08:13 +0900

    :sparkles: PlayList Service interface

---
+++ Spring/src/main/java/com/ssafy/model/service/PlaylistService.java
@@ -0,0 +1,22 @@
+package com.ssafy.model.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.model.dto.PlaylistDto;
+
+@Service
+public interface PlaylistService {
+	public List<PlaylistDto> findAllPlaylistTeacher(int start);
+
+	public List<PlaylistDto> findAllPlaylistUser(String userId);
+
+	public PlaylistDto findPlaylistDetail(int playlistId);
+
+	public PlaylistDto savePlaylist(PlaylistDto playlist);
+
+	public void deletePlaylist(int playlistId);
+
+	public PlaylistDto updatePlaylist(PlaylistDto playlist);
+}
