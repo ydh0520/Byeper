1069d694d34a586cd8198660195b4013056ac12c
김동욱 <gustkdxo007@gmail.com>
2020-11-10 17:40:29 +0900

    :art: Modified axios instance

--- frontend/src/service/axios.service.ts
+++ frontend/src/service/axios.service.ts
@@ -1,20 +1,13 @@
 import axios, { AxiosInstance } from "axios";
 
-export class LocalAxios {
-  static readonly instance: AxiosInstance = axios.create({
-    baseURL: "http://localhost:8080",
-    timeout: 5000
-  });
-}
-
 export class Axios {
   static readonly instance: AxiosInstance = axios.create({
-    baseURL: "https://k3b108.p.ssafy.io:8080",
+    baseURL: "http://k3b108.p.ssafy.io:8080",
     timeout: 10000
   });
 
-  static readonly instanceRec: AxiosInstance = axios.create({
-    baseURL: "https://k3b108.p.ssafy.io:8080",
+  static readonly instanceDjango: AxiosInstance = axios.create({
+    baseURL: "http://k3b108.p.ssafy.io:8000",
     timeout: 10000
   });
 }
@@ -23,6 +16,6 @@ Axios.instance.defaults.headers.common[
   "Authorization"
 ] = window.sessionStorage.getItem("jwt-token");
 
-Axios.instanceRec.defaults.headers.common[
+Axios.instanceDjango.defaults.headers.common[
   "Authorization"
 ] = window.sessionStorage.getItem("jwt-token");
