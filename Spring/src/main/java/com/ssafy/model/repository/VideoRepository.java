d1df86eb5205e8da5d592decdb3276a548b9113f
ydh2338 <ydh2338@naver.com>
2020-11-11 16:19:50 +0900

    :ambulance:  fix typo

--- Spring/src/main/java/com/ssafy/model/repository/VideoRepository.java
+++ Spring/src/main/java/com/ssafy/model/repository/VideoRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import com.ssafy.model.dto.Video;
 
 public interface VideoRepository extends JpaRepository<Video, String> {
-	@Query(value = "select b.* from ssafy.play as a, ssafy.video as b where a.video_id = b.video_id and a.playlist_id =:palylist_id", nativeQuery = true)
+	@Query(value = "select b.* from ssafy.play as a, ssafy.video as b where a.video_id = b.video_id and a.playlist_id =:playlist_id", nativeQuery = true)
 	public List<Video> findVideoByPlaylist(@Param("playlist_id") int playlistId);
 
 }
