26105241b8b7d85a6f822b257d9cdd215ca6a384
김동욱 <gustkdxo007@gmail.com>
2020-11-11 22:35:27 +0900

    :art: Modified django url

--- frontend/src/service/axios.service.ts
+++ frontend/src/service/axios.service.ts
@@ -7,7 +7,7 @@ export class Axios {
   });
 
   static readonly instanceDjango: AxiosInstance = axios.create({
-    baseURL: "http://k3b108.p.ssafy.io:8000",
+    baseURL: "http://k3b108.p.ssafy.io",
     timeout: 10000
   });
 }
