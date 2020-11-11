d271e51def4784e92491503704e83da919f6fc2f
ydh2338 <ydh2338@naver.com>
2020-11-11 16:10:54 +0900

    :sparkles: Add find video by playlist

--- Spring/src/main/java/com/ssafy/controller/VideoController.java
+++ Spring/src/main/java/com/ssafy/controller/VideoController.java
@@ -33,4 +33,22 @@ public class VideoController {
 
 	}
 
+	@GetMapping("/api/public/video/findplaylist")
+	public Object VideoFindbyPlaylist(@RequestParam String videoId, @RequestParam int playlistId) {
+		BasicResponse response = new BasicResponse();
+
+		response.data = videoService.FindVideobyPlaylist(playlistId);
+
+		if (response.data != null) {
+			response.status = true;
+			response.message = "조회에 성공하였습니다.";
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} else {
+			response.status = false;
+			response.message = "조회에 실패하였습니다.";
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
 }
