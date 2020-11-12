b5848d40cf30b3f56dd9ca727ecb8c87d018e159
김원호 <ho0707@naver.com>
2020-11-12 15:10:17 +0900

    :construction: add quiz page

--- frontend/src/components/lecture/Quiz.vue
+++ frontend/src/components/lecture/Quiz.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="quiz">
     <v-row class="quiz-content" align="center">
-      <v-col class="quiz-col" cols="10">
+      <v-col class="quiz-col" cols="11">
         <v-window v-model="window" vertical>
           <v-window-item v-for="Quiz in Quizes" :key="Quiz.id">
             <v-row class="quiz-text" align="center" justify="center">
+              <v-col cols="12">
               <p style="text-align: center">
                 {{ Quiz.text }}
               </p>
+              </v-col>
+              <v-col cols="3">
+                <v-text-field
+                  label="Solo"
+                  placeholder="정답:"
+                  solo
+                  width="50px"
+                ></v-text-field>
+              </v-col>
             </v-row>
           </v-window-item>
         </v-window>
@@ -18,7 +28,7 @@
           class="shrink mr-6"
           mandatory
           tag="v-flex"
-        >
+        >        
           <v-item v-for="n in length" :key="n" v-slot="{ active, toggle }">
             <div>
               <v-btn :input-value="active" icon @click="toggle">
