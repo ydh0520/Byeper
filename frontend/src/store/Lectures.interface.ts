c1e9b03ee660885b3f15ae369c338bcae8c95511
김동욱 <gustkdxo007@gmail.com>
2020-11-12 21:06:39 +0900

    :sparkles: Set lecture interface

--- frontend/src/store/Lectures.interface.ts
+++ frontend/src/store/Lectures.interface.ts
@@ -1,1 +1,23 @@
-export interface LecturesModule {}
+export interface LecturesModule {
+  lectures: Lecture[];
+  allCaptureImgs: CaptureImages[];
+}
+
+export interface Lecture {
+  video_id: string;
+  play_log: number;
+  video_description: string;
+  video_max_img: number;
+  play_complete: number;
+  video_title: string;
+  play_note: string;
+  video_img: string;
+  play_id: number;
+  playlist_id: number;
+}
+
+export interface CaptureImages {
+  address: string;
+  time: number;
+  diff: number;
+}
