d8cc3bd161db0cc9fdef36a360c3d8f6907a7438
이연재 <duseh73@gmail.com>
2020-11-13 11:44:49 +0900

    :construction: fix playlistVideos

--- frontend/src/components/main/PlayListVideos.vue
+++ frontend/src/components/main/PlayListVideos.vue
@@ -2,7 +2,7 @@
   <div class="playlistvideos">
     <v-container>
       <v-row
-        v-for="video in videoList"
+        v-for="video in PlayListVideos"
         :key="video.video_id"
         style="margin: 50px 0 50px 0"
       >
@@ -40,19 +40,12 @@
         <v-col cols="1"></v-col>
 
         <v-col cols="5" justify="center">
-          <div class="card">
-            <div class="imgBx">
-              <img class="bigimg" :src="video.video_img" />
-              <img
-                class="smallimg"
-                src="@/assets/playBtn.png"
-                @click="goLecture(video.video_title)"
-              />
-            </div>
-            <div class="content">
-              <img :src="video.video_img" />
-            </div>
-          </div>
+          <iframe
+            :src="`https://youtube.com/embed/${video.video_id}`"
+            width="400px"
+            height="225px"
+            frameborder="0"
+          />
         </v-col>
       </v-row>
     </v-container>
@@ -60,38 +53,21 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 import { namespace } from "vuex-class";
-import { Lecture } from "../../store/Lectures.interface";
-import { PlayListVideos } from "../../store/PlayList.interface";
+import { PlayList } from "../../store/PlayList.interface";
 
 const PlayListModule = namespace("PlayListModule");
-const LecturesModule = namespace("LecturesModule");
 
 @Component
 export default class PlayListRoadmap extends Vue {
-  @PlayListModule.State PlayListVideos!: PlayListVideos[];
-  @LecturesModule.State lectures!: Lecture[];
-
-  videoList: PlayListVideos[] | Lecture[] = [];
+  @PlayListModule.State PlayListVideos!: [];
 
   goLecture(courseName: string) {
     this.$router.push({
       name: "LecturePage",
       params: { courseName: courseName }
     });
-  }
-  setVideoList() {
-    const routeName = this.$route.name;
-    console.log(routeName);
-    if (routeName === "PlayList") {
-      this.videoList = this.PlayListVideos;
-    } else if (routeName === "LectureListPage") {
-      this.videoList = this.lectures;
-    }
-  }
-  created() {
-    this.setVideoList();
   }
 }
 </script>
@@ -118,12 +94,7 @@ export default class PlayListRoadmap extends Vue {
   flex-wrap: wrap;
   margin: 20px;
 }
-.container .card {
-  position: relative;
-  height: 225px;
-  display: flex;
-  width: 400px;
-}
+
 .videodescription {
   position: relative;
   height: 250px;
@@ -142,95 +113,5 @@ export default class PlayListRoadmap extends Vue {
 .videodescription .content h3 {
   margin-bottom: 5px;
   font-size: 24px;
-}
-
-.card .imgBx {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: #333;
-  z-index: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-  transition: 0.5s ease-in-out;
-}
-.card:hover .imgBx {
-  width: 100px;
-  height: 100px;
-  left: -50px;
-  top: calc(50% - 50px);
-  transition: 0.5s ease-in-out;
-  background: #ff00ff00;
-}
-.card .smallimg {
-  display: none;
-}
-.card:hover .smallimg {
-  display: block;
-  cursor: pointer;
-}
-.card:hover .bigimg {
-  display: none;
-}
-.card .imgBx img {
-  max-height: 100%;
-  transition: 0.5s ease-in-out;
-}
-
-.card .content {
-  position: absolute;
-  width: calc(100% - 50px);
-  height: 100%;
-  padding: 0px;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-}
-.card .content img {
-  width: 400px;
-  height: 225px;
-}
-@keyframes fadeInBottom {
-  0% {
-    transform: translateY(20px);
-    opacity: 0;
-  }
-  100% {
-    transform: translateY();
-    opacity: 1;
-  }
-}
-@media (max-width: 992px) {
-  .card {
-    width: 400px;
-  }
-}
-@media (max-width: 768px) {
-  .card {
-    max-width: 300px;
-    flex-direction: column;
-    height: auto;
-  }
-  .card .imgBx {
-    position: relative;
-  }
-  .card .imgBx,
-  .card:hover .imgBx {
-    width: 100%;
-    height: 200px;
-    left: 0;
-  }
-  .card .imgBx img,
-  .card:hover .imgBx img {
-    max-width: 100px;
-  }
-  .card .content {
-    position: relative;
-    width: 100%;
-  }
 }
 </style>
