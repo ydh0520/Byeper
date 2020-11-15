cea38d33ce96f7d5a1b3482d1209bf750ae27b0b
김동욱 <gustkdxo007@gmail.com>
2020-11-15 20:20:11 +0900

    :sparkles: Add curriculum page layout

--- frontend/src/components/lecture/Curriculum.vue
+++ frontend/src/components/lecture/Curriculum.vue
@@ -1,12 +1,97 @@
 <template>
-  <div>커리큘럼</div>
+  <div class="list-page">
+    <h2>재생목록 이름</h2>
+    <div>진도율: 5강/20강(25%) | 시간: 100분/300분</div>
+    <v-progress-linear color="deep-orange" height="12" value="25">
+      <strong style="font-size: 0.8em">25%</strong>
+    </v-progress-linear>
+    <h1>목차</h1>
+    <v-list two-line>
+      <v-list-item-group v-model="selected" active-class="pink--text" multiple>
+        <template v-for="(item, index) in items">
+          <v-list-item :key="item.title">
+            <template v-slot:default="{ active }">
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+
+                <v-list-item-subtitle
+                  class="text--primary"
+                  v-text="item.headline"
+                ></v-list-item-subtitle>
+
+                <v-list-item-subtitle
+                  v-text="item.subtitle"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-list-item-action-text
+                  v-text="item.action"
+                ></v-list-item-action-text>
+
+                <v-icon v-if="!active" color="grey lighten-1">
+                  mdi-star-outline
+                </v-icon>
+
+                <v-icon v-else color="yellow darken-3">
+                  mdi-star
+                </v-icon>
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+
+          <v-divider v-if="index < items.length - 1" :key="index"></v-divider>
+        </template>
+      </v-list-item-group>
+    </v-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class Curriculum extends Vue {}
+export default class Curriculum extends Vue {
+  selected = 2;
+  items = [
+    {
+      action: "15 min",
+      headline: "Brunch this weekend?",
+      subtitle:
+        "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+      title: "Ali Connors"
+    },
+    {
+      action: "2 hr",
+      headline: "Summer BBQ",
+      subtitle: "Wish I could come, but I'm out of town this weekend.",
+      title: "me, Scrott, Jennifer"
+    },
+    {
+      action: "6 hr",
+      headline: "Oui oui",
+      subtitle: "Do you have Paris recommendations? Have you ever been?",
+      title: "Sandra Adams"
+    },
+    {
+      action: "12 hr",
+      headline: "Birthday gift",
+      subtitle:
+        "Have any ideas about what we should get Heidi for her birthday?",
+      title: "Trevor Hansen"
+    },
+    {
+      action: "18hr",
+      headline: "Recipe to try",
+      subtitle: "We should eat this: Grate, Squash, Corn, and tomatillo Tacos.",
+      title: "Britta Holt"
+    }
+  ];
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.list-page {
+  color: black;
+}
+</style>
