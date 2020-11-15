17c88b98558f7e57762203c693c7528bbd7cf5eb
김동욱 <gustkdxo007@gmail.com>
2020-11-15 22:04:25 +0900

    :sparkles: Connect server in curriculum

--- frontend/src/components/lecture/Curriculum.vue
+++ frontend/src/components/lecture/Curriculum.vue
@@ -1,42 +1,46 @@
 <template>
-  <div class="list-page">
-    <h2>재생목록 이름</h2>
-    <div>진도율: 5강/20강(25%) | 시간: 100분/300분</div>
-    <v-progress-linear color="deep-orange" height="12" value="25">
-      <strong style="font-size: 0.8em">25%</strong>
-    </v-progress-linear>
-    <h1>목차</h1>
-    <v-list two-line>
-      <v-list-item-group v-model="selected" active-class="pink--text" multiple>
-        <template v-for="(item, index) in items">
-          <v-list-item :key="item.title">
-            <template v-slot:default="{ active }">
-              <v-list-item-content>
-                <v-list-item-title v-text="item.title"></v-list-item-title>
-
-                <v-list-item-subtitle
-                  class="text--primary"
-                  v-text="item.headline"
-                ></v-list-item-subtitle>
-
-                <v-list-item-subtitle
-                  v-text="item.subtitle"
-                ></v-list-item-subtitle>
-              </v-list-item-content>
-
-              <v-list-item-action>
-                <v-list-item-action-text
-                  v-text="item.action"
-                ></v-list-item-action-text>
-
-                <v-icon v-if="!active" color="grey lighten-1">
+  <div v-if="lectures.length">
+    <div class="list-page">
+      <h1>{{ $route.params.playlistTitle }}</h1>
+      <div>진도율: {{ completedLecture }}강 / {{ lectures.length }}강</div>
+      <v-progress-linear color="deep-orange" height="12" :value="progress">
+        <strong style="font-size: 0.8em">{{ progress }}%</strong>
+      </v-progress-linear>
+    </div>
+    <h2 class="cur-box">목차</h2>
+    <v-list class="pt-0" two-line>
+      <v-list-item-group v-model="selected" active-class="pink--text">
+        <template v-for="(item, index) in lectures">
+          <v-list-item :key="item.video_id">
+            <template>
+              <v-list-item-icon>
+                <v-icon v-if="item.play_complete === 0" color="grey lighten-1">
                   mdi-star-outline
                 </v-icon>
 
                 <v-icon v-else color="yellow darken-3">
                   mdi-star
                 </v-icon>
-              </v-list-item-action>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title
+                  v-text="item.video_title"
+                ></v-list-item-title>
+
+                <v-list-item-subtitle
+                  class="text--primary"
+                  v-text="
+                    `진행률 : ${
+                      item.play_complete === 1 ? 100 : item.play_log
+                    }%`
+                  "
+                ></v-list-item-subtitle>
+
+                <!-- <v-list-item-subtitle
+                  v-text="item.subtitle"
+                ></v-list-item-subtitle> -->
+              </v-list-item-content>
             </template>
           </v-list-item>
 
@@ -49,9 +53,26 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { namespace } from "vuex-class";
+import { Lecture } from "../../store/Lectures.interface";
+
+const LecturesModule = namespace("LecturesModule");
 
 @Component
 export default class Curriculum extends Vue {
+  @LecturesModule.State lecture!: Lecture;
+  @LecturesModule.State lectures!: Lecture[];
+  @LecturesModule.Action FETCH_LECTURE_BY_COURSE: any;
+
+  get completedLecture() {
+    return this.lectures.filter(lec => lec.play_complete === 1).length;
+  }
+  get progress() {
+    return Number(
+      ((this.completedLecture / this.lectures.length) * 100).toFixed(1)
+    );
+  }
+
   selected = 2;
   items = [
     {
@@ -94,11 +115,23 @@ export default class Curriculum extends Vue {
     editorContainer.style.backgroundColor = "#1E1E1E";
     editorContainer.style.paddingBottom = "0";
   }
+  created() {
+    this.FETCH_LECTURE_BY_COURSE(this.$route.params.playlistId);
+  }
 }
 </script>
 
 <style scoped>
 .list-page {
-  color: black;
+  margin: 30px 30px;
+  color: white;
+}
+.cur-box {
+  border-top: 2px solid grey;
+  border-bottom: 2px solid grey;
+  background-color: #757575;
+  margin-bottom: 0px;
+  line-height: 50px;
+  padding: 10px;
 }
 </style>
