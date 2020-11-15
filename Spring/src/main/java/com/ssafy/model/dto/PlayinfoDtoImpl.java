cca363b9face6f65afc67753e74678988835eb41
ydh2338 <ydh2338@naver.com>
2020-11-15 17:40:04 +0900

    :art: Fix playinfo update

---
+++ Spring/src/main/java/com/ssafy/model/dto/PlayinfoDtoImpl.java
@@ -0,0 +1,105 @@
+package com.ssafy.model.dto;
+
+public class PlayinfoDtoImpl {
+
+	int play_id;
+
+	int play_log;
+
+	int playlist_id;
+
+	int play_complete;
+
+	String play_note;
+
+	String video_id;
+
+	String video_title;
+
+	String video_description;
+
+	String video_img;
+
+	int video_max_img;
+
+	public int getPlay_id() {
+		return play_id;
+	}
+
+	public void setPlay_id(int play_id) {
+		this.play_id = play_id;
+	}
+
+	public int getPlay_log() {
+		return play_log;
+	}
+
+	public void setPlay_log(int play_log) {
+		this.play_log = play_log;
+	}
+
+	public int getPlaylist_id() {
+		return playlist_id;
+	}
+
+	public void setPlaylist_id(int playlist_id) {
+		this.playlist_id = playlist_id;
+	}
+
+	public int getPlay_complete() {
+		return play_complete;
+	}
+
+	public void setPlay_complete(int play_complete) {
+		this.play_complete = play_complete;
+	}
+
+	public String getPlay_note() {
+		return play_note;
+	}
+
+	public void setPlay_note(String play_note) {
+		this.play_note = play_note;
+	}
+
+	public String getVideo_id() {
+		return video_id;
+	}
+
+	public void setVideo_id(String video_id) {
+		this.video_id = video_id;
+	}
+
+	public String getVideo_title() {
+		return video_title;
+	}
+
+	public void setVideo_title(String video_title) {
+		this.video_title = video_title;
+	}
+
+	public String getVideo_description() {
+		return video_description;
+	}
+
+	public void setVideo_description(String video_description) {
+		this.video_description = video_description;
+	}
+
+	public String getVideo_img() {
+		return video_img;
+	}
+
+	public void setVideo_img(String video_img) {
+		this.video_img = video_img;
+	}
+
+	public int getVideo_max_img() {
+		return video_max_img;
+	}
+
+	public void setVideo_max_img(int video_max_img) {
+		this.video_max_img = video_max_img;
+	}
+
+}
